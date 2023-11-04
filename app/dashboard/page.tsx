d8765418eec5856c1151e24ec50c5e49a4729205
Julio Saito <saitodisse@gmail.com>
2023-11-04 04:08:13 -0300

    remove console

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -6,7 +6,6 @@ import { fetchRevenue } from '@/app/lib/data'
 
 export default async function Page() {
 	const revenue = await fetchRevenue()
-	console.log('--  revenue: ', revenue)
 	return (
 		<main>
 			<h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
