b58505b82081a2732928780d93a0a5b41866b60c
Julio Saito <saitodisse@gmail.com>
2023-11-04 03:40:22 -0300

    dashboard pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ export default function RootLayout({
 	children: React.ReactNode
 }) {
 	return (
-		<html lang='pt'>
+		<html lang='en'>
 			<body className={`${inter.className} antialiased`}>{children}</body>
 		</html>
 	)
